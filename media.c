8922dc2d350784cd8086dd0f2ded54b108a79b1e
linamgr <32674289+linamgr@users.noreply.github.com>
2021-04-24 20:38:46 -0300

    Create media.c

---
+++ media.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+#include <stdlib.h>
+
+int main(){
+  int x,y;
+  scanf("Digite o valor do primeiro número: %d", x);
+  scanf("Digite o valor do segundo número: %d", y);
+
+  media = (x+y)/2;
+
+  printf("A media dos números é:%d", media);
+}
