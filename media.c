f639b013e8113663044a007607fcc75331e6cd31
linamgr <32674289+linamgr@users.noreply.github.com>
2021-04-24 21:46:27 -0300

    Update media.c

--- media.c
+++ media.c
@@ -1,12 +1,22 @@
-#include <studio.h>
-#include <stdlib.h>
+#include <stdio.h>
 
-int main(){
-  int x,y;
-  scanf("Digite o valor do primeiro número: %d", x);
-  scanf("Digite o valor do segundo número: %d", y);
-
-  media = (x+y)/2;
-
-  printf("A media dos números é:%d", media);
+int main(void) {
+  //define os dados de entrada
+   float x,y;
+   //define os dados de saída
+   float media;
+   
+   //escreve na tela
+   printf("Digite o valor do primeiro número:");
+   //lê o valor da tela
+   scanf("%f", &x);
+   //escreve na tela
+   printf("Digite o valor do segundo número:");
+   //lê o valor da tela
+   scanf("%f", &y);
+   //calcula a media aritmética dos números lidos
+   media = (x+y)/2;
+  //escreve na tela o resultado
+   printf("A media dos números é:%f", media);
+   return 0;
 }
